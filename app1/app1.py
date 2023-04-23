acf1e6af51b7ee7743ae6b885f042e53c5079ee4
Harry Park <38964680+HarryPark1108@users.noreply.github.com>
2023-04-23 13:16:26 +0900

    add heading

--- app1/app1.py
+++ app1/app1.py
@@ -26,30 +26,33 @@ class State(pc.State):
 def index():
     # We use different components
     # such as pc.hstack, pc.button, and pc.heading to build the frontend.
-    return pc.hstack(
-        pc.button(
-            "Decrement",
-            color_scheme="red",
-            border_radius="1em",
-            on_click=State.decrement,
-        ),
-        # The pc.heading component displays the current value of the counter by referencing State.count.
-        # All components that reference state will reactively update whenever the state changes.
-        pc.heading(State.count, font_size="2em"),
+    return pc.vstack(
+        pc.heading("TEST"),
+        pc.hstack(
+            pc.button(
+                "Decrement",
+                color_scheme="red",
+                border_radius="1em",
+                on_click=State.decrement,
+            ),
+            # The pc.heading component displays the current value of the counter by referencing State.count.
+            # All components that reference state will reactively update whenever the state changes.
+            pc.heading(State.count, font_size="2em"),
 
-        # Components interact with the state by binding events triggers to event handlers.
-        # For example, on_click is an event that is triggered when a user clicks a component.
-        pc.button(
-            "Increment",
-            color_scheme="green",
-            border_radius="1em",
-            on_click=State.increment,
+            # Components interact with the state by binding events triggers to event handlers.
+            # For example, on_click is an event that is triggered when a user clicks a component.
+            pc.button(
+                "Increment",
+                color_scheme="green",
+                border_radius="1em",
+                on_click=State.increment,
+            )
+            # Components can be nested to create complex layouts,
+            # and can be styled using the full power of CSS.
+
+            # The first button in our app binds its on_click event to the State.decrement event handler,
+            # and the second button binds its to the State.increment handler.
         )
-        # Components can be nested to create complex layouts,
-        # and can be styled using the full power of CSS.
-
-        # The first button in our app binds its on_click event to the State.decrement event handler,
-        # and the second button binds its to the State.increment handler.
     )
 
 
